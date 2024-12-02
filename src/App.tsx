6fa1eb0d772c5bce6c76ac946c8cd3ac70dbd4fa
msr <mshineray@qq.com>
2024-12-02 20:48:56 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+export default function App() {
+    return (
+        <header>
+            hi
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </header>
+    );
+}
